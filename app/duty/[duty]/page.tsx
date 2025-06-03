74f8abe8c35cb76f23cd777ba4c2f0a64057b254
agb220 <annabalabonchik@gmail.com>
2025-06-03 19:48:17 +0300

    update delete duty

--- app/duty/[duty]/page.tsx
+++ app/duty/[duty]/page.tsx
@@ -11,8 +11,11 @@ export default function DutyPage({ params }: DutyPageProps) {
   return (
     <div className="container">
       <div className="py-10 flex flex-col gap-10">
-        <Link href="/" className="px-5 py-2 bg-indigo-400 rounded max-w-fit">
-          Назад
+        <Link
+          href="/"
+          className="px-5 py-2 bg-indigo-400 rounded max-w-fit hover:bg-indigo-500 transition-all duration-300 hover:text-white"
+        >
+          Back
         </Link>
         <CalendarWrapper dutyId={duty} />
       </div>
--- components/duties/Itemduty.tsx
+++ components/duties/Itemduty.tsx
@@ -1,9 +1,11 @@
 "use client";
-import { deleteDuty } from "@/app/actions";
+import { useState } from "react";
 import WeekSchedule from "../calendar/WeekSchedule";
 import Button from "../common/Button";
 import { Duty } from "@/app/page";
+import { deleteDuty } from "@/app/actions";
 import { useAuth } from "@/context/AuthContext";
+import ConfirmModal from "../modals/ConfirmModal";
 
 interface ItemDutyProps {
   duty: Duty;
@@ -12,6 +14,8 @@ interface ItemDutyProps {
 
 const ItemDuty = (props: ItemDutyProps) => {
   const { user } = useAuth();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleClick = async (id: string) => {
     props.setIsLoading(true);
@@ -28,10 +32,17 @@ const ItemDuty = (props: ItemDutyProps) => {
         </span>
         <Button
           img={"/images/delete.svg"}
-          onClick={() => handleClick(props.duty.id)}
+          onClick={() => setIsModalOpen(true)}
         />
       </div>
       <WeekSchedule duty={props.duty} />
+      <ConfirmModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onConfirm={() => handleClick(props.duty.id)}
+        title="Deletion"
+        message="Are you sure you want to delete duty?"
+      />
     </div>
   );
 };
